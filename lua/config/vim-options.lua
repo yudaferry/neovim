0d684e42b838cde7048c36e112c783221550a950
Yuda Ferry Mahendra <yuda.mahendra@shark.tech>
2024-10-29 21:26:08 +0700

    inital setup

---
+++ lua/config/vim-options.lua
@@ -0,0 +1,9 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set nu")
+vim.cmd("set rnu")
+vim.cmd("set foldmethod=syntax")
+
+vim.g.mapleader = " "
